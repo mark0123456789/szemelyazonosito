ab2aec416c0031a9b0c38a1f6a65d59bf73b6e3a
mark <piringerm@kkszki.hu>
2024-10-25 12:21:59 +0200

    az adatok kidolgozása

--- Program.cs
+++ Program.cs
@@ -9,23 +9,29 @@ namespace személyazonosító
     internal class Program
     {
 
-        public static int[] tizszam = new int[2];
+        public static string[] tizszam = new string[2];
         public static void bekeres()
         {
-            Console.WriteLine("kérem a személyi azonosítód első 10 számjegyét: ");
-            tizszam[0] = Convert.ToInt16(Console.ReadLine());
+            while (tizszam.Length != 10)
+            {
+                Console.WriteLine("kérem a személyazonosítolyának az első 10 számjegyét");
+                tizszam[0] = Console.ReadLine();
+            }
         }
         public static void kiiratas() 
         { 
-            int szam = tizszam[0];
-            int kor = 
-            if (szam == )
+            string Tizszam = tizszam[0];
+            int nem = Convert.ToInt32(Tizszam[0]);
+            int kor =  ;
+            int sorszam = Convert.ToInt32(Tizszam[7]);
+
+            if (nem  == 0%2)
             {
-                Console.WriteLine("Maga egy férfi");
+                Console.WriteLine("Maga egy nő ");
             }
             else 
             { 
-                Console.WriteLine("Maga egy Nő"); 
+                Console.WriteLine("Maga egy férfi "); 
             }
             DateTime
             Console.WriteLine("$ magának a {} a születési sorszáma");
