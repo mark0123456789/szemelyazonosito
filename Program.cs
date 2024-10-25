1333dce9f400de48ccbe16f7140cc50e836ba545
mark <piringerm@kkszki.hu>
2024-10-25 12:03:33 +0200

    készül az első ki iratás

--- Program.cs
+++ Program.cs
@@ -10,15 +10,31 @@ namespace személyazonosító
     {
 
         public static int[] tizszam = new int[2];
-        public static DateTime[] evszamok;
         public static void bekeres()
         {
             Console.WriteLine("kérem a személyi azonosítód első 10 számjegyét: ");
             tizszam[0] = Convert.ToInt16(Console.ReadLine());
-
+        }
+        public static void kiiratas() 
+        { 
+            int szam = tizszam[0];
+            int kor = 
+            if (szam == )
+            {
+                Console.WriteLine("Maga egy férfi");
+            }
+            else 
+            { 
+                Console.WriteLine("Maga egy Nő"); 
+            }
+            DateTime
+            Console.WriteLine("$ magának a {} a születési sorszáma");
+            Console.WriteLine("$ magának a {kor}. születés napja van ebben az évben");
         }
         static void Main(string[] args)
         {
+            bekeres();
+            kiiratas(); 
         }
     }
 }
