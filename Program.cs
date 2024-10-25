763c6b671776a9b6835266f35b520ce9d6ff16df
mark <piringerm@kkszki.hu>
2024-10-25 11:40:48 +0200

    kész az első bekérés

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace személyazonosító
+{
+    internal class Program
+    {
+
+        public static int[] tizszam = new int[2];
+        public static DateTime[] evszamok;
+        public static void bekeres()
+        {
+            Console.WriteLine("kérem a személyi azonosítód első 10 számjegyét: ");
+            tizszam[0] = Convert.ToInt16(Console.ReadLine());
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
